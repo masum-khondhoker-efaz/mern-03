bb658d6710d8516f82ca0251e0b5d4a167f8f192
Masum Khondhoker Efaz <efazkh@gmail.com>
2024-08-28 12:20:14 +0600

    All done

--- server/app/config/config.js
+++
@@ -1,7 +0,0 @@
-export const PORT = 5500;
-export const MAX_JSON_SIZE = "50mb";
-export const URL_ENCODED = true;
-export const REQUEST_LIMIT_TIME = 15 * 60 * 1000; // 15 ms
-export const REQUEST_LIMIT_NUMBER = 3000; // per 15 min 3000 request
-export const WEB_CACHE = false;
-export const MONGODB_CONNECTION = ""
